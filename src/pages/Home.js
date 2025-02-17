3b73fd59f54d5e5456cd56d1f0c7231d7006b983
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-02-17 16:14:57 +0400

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./Home.css"
-import Results from './Results';
 import RaceInfo from './RaceInfo';
 
 const Home = () => {
   return (
     <div>
-      {/* <Results /> */}
       <RaceInfo />
     </div>
   );
